d8f7c86da91efd6872dfaa67d3ef75ce9015f545
Ivan Pavlov <ivan.pavlov@globallogic.com>
2018-06-25 18:28:53 +0300

    Change: added XCTAsset(false) to UITests

--- TestTVAppUITests/TestTVAppUITests.swift
+++ TestTVAppUITests/TestTVAppUITests.swift
@@ -32,7 +32,7 @@ class TestTVAppUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        XCTFail()
+        XCTAssert(false)
     }
     
 }
